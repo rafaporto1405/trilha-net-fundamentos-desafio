5e5e870bc61377868e6e94f5fa51f25104a8acb4
Rafael Porto <rafaporto1405@gmail.com>
2023-11-10 00:12:12 -0300

    Update Estacionamento.cs
    correção da classe estacionamento

--- DesafioFundamentos/Models/Estacionamento.cs
+++ DesafioFundamentos/Models/Estacionamento.cs
@@ -1,3 +1,5 @@
+using System.Text.RegularExpressions;
+
 namespace DesafioFundamentos.Models
 {
     public class Estacionamento
@@ -5,6 +7,12 @@ namespace DesafioFundamentos.Models
         private decimal precoInicial = 0;
         private decimal precoPorHora = 0;
         private List<string> veiculos = new List<string>();
+
+        static bool ValidarPlaca(string placa)
+        {
+            string pattern = @"^[A-Za-z]{3}\d{1}[A-Za-z0-9]{1}\d{2}$";
+            return Regex.IsMatch(placa, pattern);
+        }
 
         public Estacionamento(decimal precoInicial, decimal precoPorHora)
         {
@@ -14,34 +22,47 @@ namespace DesafioFundamentos.Models
 
         public void AdicionarVeiculo()
         {
-            // TODO: Pedir para o usuário digitar uma placa (ReadLine) e adicionar na lista "veiculos"
-            // *IMPLEMENTE AQUI*
             Console.WriteLine("Digite a placa do veículo para estacionar:");
+            string placa = Console.ReadLine().ToUpper();
+
+            if (ValidarPlaca(placa))
+            {
+                veiculos.Add(placa);
+                Console.WriteLine($"Entrada do veículo de placa: {placa} confirmada!");
+            }
+            else
+            {
+                Console.WriteLine($"Placa {placa} não reconhecida, verifique a placa digitada!");
+            }
         }
 
         public void RemoverVeiculo()
         {
+            if (!veiculos.Any())
+            {
+                Console.WriteLine("Não há veículos estacionados.");
+                return;
+            }
+
             Console.WriteLine("Digite a placa do veículo para remover:");
+            string placa = Console.ReadLine().ToUpper();
 
-            // Pedir para o usuário digitar a placa e armazenar na variável placa
-            // *IMPLEMENTE AQUI*
-            string placa = "";
-
-            // Verifica se o veículo existe
             if (veiculos.Any(x => x.ToUpper() == placa.ToUpper()))
             {
                 Console.WriteLine("Digite a quantidade de horas que o veículo permaneceu estacionado:");
 
-                // TODO: Pedir para o usuário digitar a quantidade de horas que o veículo permaneceu estacionado,
-                // TODO: Realizar o seguinte cálculo: "precoInicial + precoPorHora * horas" para a variável valorTotal                
-                // *IMPLEMENTE AQUI*
-                int horas = 0;
-                decimal valorTotal = 0; 
-
-                // TODO: Remover a placa digitada da lista de veículos
-                // *IMPLEMENTE AQUI*
-
-                Console.WriteLine($"O veículo {placa} foi removido e o preço total foi de: R$ {valorTotal}");
+                if (int.TryParse(Console.ReadLine(), out int horas))
+                {
+                    decimal valorTotal = (precoInicial + precoPorHora) * horas;
+                    veiculos.Remove(placa);
+                    Console.WriteLine($"O veículo {placa} foi removido e o preço total foi de: R$ {valorTotal}");
+                    return;
+                }
+                else
+                {
+                    Console.WriteLine("Por favor, insira um valor numérico válido para a quantidade de horas.");
+                    return;
+                }
             }
             else
             {
@@ -51,12 +72,13 @@ namespace DesafioFundamentos.Models
 
         public void ListarVeiculos()
         {
-            // Verifica se há veículos no estacionamento
             if (veiculos.Any())
             {
                 Console.WriteLine("Os veículos estacionados são:");
-                // TODO: Realizar um laço de repetição, exibindo os veículos estacionados
-                // *IMPLEMENTE AQUI*
+                veiculos.ForEach((veiculo) =>
+                {
+                    Console.WriteLine($"* {veiculo}\n");
+                });
             }
             else
             {
